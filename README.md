fd8e1a097b62103b7d4c75631a076db3c5fdd8a1
abdullahshahid137 <abdullahshahid137@gmail.com>
2022-08-29 20:21:29 +0500

    add a new file index-1.2.js

---
+++ README.md
@@ -0,0 +1,22 @@
+# SolanaToken
+
+
+install node packegs using ```yarn install```.
+all accounts are available in ```wallet``` folder.
+run code using `node index-1.2.js`
+
+in ```index-1.2``` we add token distribution function.
+
+1. create token using ```createToken``` that will give the mint address
+2. get token detail using  ```tokenSupply``` this will give the total supplay of that mint address
+3. create Associated Account using ```CreateAssociatedAccount``` this will create the associated account for that token
+4. create account info using ```GetAccountInfo```
+5. mint token using ```mintTokens``` can mint the token of that mint address to the token associated account for any wallet address
+6. transfer token using ```transferTokens``` transfer token from a token associated account to other(receiver) token associated account
+7. burn token using ```burnTokens``` burn token from a token associated account
+8. token send to a receiver account and  distribute tokens to given accounts using ```tokenSendAndDistribute``` itt will create a tokenassociated account if account does not exist and sent tokens to other token associated accounts
+9. get the token ststus of all given accounts using```getALlAccountsTokenStatus``` and can resend token to main account
+10. get kepair from json file using```get_kaypair_from_json``` this will return the keypair
+
+
+
---
+++ index-1.2.js
@@ -0,0 +1,208 @@
+const  { createMint , getMint ,getOrCreateAssociatedTokenAccount,getAccount,mintTo,transfer,burn} = require('@solana/spl-token');
+const { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL ,PublicKey, TransactionSignature} =  require('@solana/web3.js');
+const base58 = require('bs58');
+const fs = require('fs');
+
+
+// get kepair from json file
+const get_kaypair_from_json = (file_path) => {
+  const u8_array = JSON.parse(fs.readFileSync(file_path))
+  return Keypair.fromSeed(Uint8Array.from(u8_array.slice(0,32)));
+}
+
+//9mkZPFisGaJW7mcd3GybGHwTn21XaMjTihjAK7MDACXX
+const payer = get_kaypair_from_json('wallet/my-keypair.json');
+const mintAuthority = payer
+const freezeAuthority = payer
+
+
+
+//connection with solana
+const connection = new Connection(
+  clusterApiUrl('devnet'),
+  'confirmed'
+);
+
+// test conneection
+// (async ()=>{
+//   console.log(await connection.getBalance(payer.publicKey));
+
+// })()
+
+//Token Minting Account Address
+const mint = new PublicKey("2PmCJRYKGTakTaY3n5SgQqA5tFWptUQ18WK3VZR1fPch");
+//token address after calling "CreateAssociatedAccount"
+const tokenAccount = new PublicKey("BjvDdrysS119JmtoenfV2WUegMXogWJRTJpH4W7Qk41F");
+
+//wallet addresses
+const recieverAccount = new PublicKey("5qrUGR4BP7wp3g9TWkL8xHeTAMtQQxEY96RpAyUanwPt"); //account 1
+const devteamAccount = new PublicKey("41KZXHc3szQrVCUHwJs4Lmi6qQu4wkUqYY66UpiWeunB");  //account 2
+const stakeholdersAccount = new PublicKey("7sSLC9SxjsK1w2X8tpFxTVi15XfVxvunRJYYRJut2PJg");  //account 3
+const charityAccount = new PublicKey("DSZ3B5u2NxfatHGwPH5DcFLf6XZY3PgP5chEndeCPbdf"); //Account 4
+
+const addresses = {recieverAccount, devteamAccount, stakeholdersAccount, charityAccount};
+
+//Create Token (give mint address)
+const createToken = async ()=> {
+    const mint = await createMint(
+        connection,
+        payer,
+        mintAuthority.publicKey,
+        freezeAuthority.publicKey,
+        9, // We are using 9 to match the CLI decimal default exactly
+        await get_kaypair_from_json('wallet/mint.json')
+      );
+      console.log(mint.toBase58());      // print mint address
+}
+
+//get Token Supply
+const tokenSupply = async () => {
+    // console.log("Mint result : ",mint)
+    const mintInfo = await getMint(
+        connection,
+        mint
+      )      
+      console.log(mintInfo.supply)
+    
+}
+
+
+//create Token Associated account
+const treateAssociatedAccount = async (owner) => {
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        payer,
+        mint,
+        owner
+      )
+      console.log("Mint Address:", tokenAccount.mint.toBase58());
+      console.log("Owner Address: ", tokenAccount.owner.toBase58());
+      console.log("Token Account:", tokenAccount.address.toBase58());
+      return tokenAccount.address;
+}
+//Get Account Info
+const GetAccountInfo = async (tokenAccount) => {
+  const tokenAccountInfo = await getAccount(
+    connection,
+    tokenAccount
+  )
+  
+  console.log(tokenAccountInfo.amount);
+}
+
+
+//mint_tokens in token associated account
+const mintTokens = async (tokenAccount,amount) => {
+  await mintTo(
+    connection,
+    payer,
+    mint,
+    tokenAccount,
+    payer.publicKey,
+    amount
+  )
+}
+
+// burn_tokens
+const burnTokens = async (tokenAccount,amount) => {
+  result = await burn(connection,
+   payer,
+   tokenAccount,
+   mint,
+   payer.publicKey,
+   amount)
+   console.log("result : ",result);
+ }
+
+
+
+
+//send Tokens to Other address
+const transferTokens = async (signer, senderTokenAccount, recieverAccount, amount) => {
+  const toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, payer, mint, recieverAccount);
+  console.log("Associated Token Account: ", toTokenAccount.address.toBase58());
+  signature = await transfer(
+    connection,
+    signer,
+    senderTokenAccount,
+    toTokenAccount.address,
+    signer.publicKey,
+    amount,
+  );
+    return signature;
+}
+
+//mint and distribute to different people
+const tokenSendAndDistribute  = async (tokenAccount, amount, addresses) => {
+
+  //Token Distribution 
+  forcharity = 3 * amount / 100; // 30 out of 1000
+  forDevTeam = 10 * amount / 100; // 100 out of 1000
+  forStakeHolder =  2 * amount / 100; 20
+  forReciever = amount - (forcharity + forDevTeam + forStakeHolder); //850 out of 1000
+
+  
+   let txt = await transferTokens(payer, tokenAccount, addresses.recieverAccount, forReciever);  
+    console.log("recieverAccount transactions : ", txt);
+     txt = await transferTokens(payer, tokenAccount, addresses.charityAccount, forcharity);  
+    console.log("charityAddress transactions : ", txt);
+     txt = await transferTokens(payer, tokenAccount, addresses.devteamAccount, forDevTeam);  
+    console.log("devteamAddress transactions : ", txt);
+     txt = await transferTokens(payer, tokenAccount, addresses.stakeholdersAccount, forStakeHolder);  
+    console.log("stakeholdersAddress transactions : ", txt);
+
+    const send_tokens = getAccountInfo(await createAssociatedAccount(addresses.recieverAccount));
+    console.log("send token to reciever : ", send_tokens);
+
+}
+
+
+// get all accounts token status
+// send token back
+const getALlAccountsTokenStatus = async () => {
+  // await transferTokens( get_kaypair_from_json('wallet/recieverAccount.json'),
+  //   await createAssociatedAccount(addresses.recieverAccount), 
+  //   payer.publicKey, 850);
+
+  // await transferTokens( get_kaypair_from_json('wallet/charityAccount.json'),
+  //   await createAssociatedAccount(addresses.charityAccount), 
+  //   payer.publicKey, 30);
+
+  // await transferTokens( get_kaypair_from_json('wallet/devteamAccount.json'),
+  //   await createAssociatedAccount(addresses.devteamAccount), 
+  //   payer.publicKey, 100);
+
+  // await transferTokens( get_kaypair_from_json('wallet/stakeholdersAccount.json'),
+  //   await createAssociatedAccount(addresses.stakeholdersAccount), 
+  //   payer.publicKey, 20);
+
+
+  await getAccountInfo(await createAssociatedAccount(payer.publicKey));
+  await getAccountInfo(await createAssociatedAccount(addresses.recieverAccount));
+  await getAccountInfo(await createAssociatedAccount(addresses.charityAccount));
+  await getAccountInfo(await createAssociatedAccount(addresses.devteamAccount));
+  await getAccountInfo(await createAssociatedAccount(addresses.stakeholdersAccount));
+}
+
+
+(async () => {
+  // createToken();
+  // tokenSupply();
+  // createAssociatedAccount(payer.publicKey);
+  // getAccountInfo(tokenAccount);
+  // mintTokens(tokenAccount, 10e9);
+  // burnTokens(tokenAccount, 1e9);
+
+  // addresses.recieverAccount = recieverAccount;
+  await tokenSendAndDistribute(tokenAccount, 1000, addresses);
+  await getALlAccountsTokenStatus();
+})();
+
+
+
+
+
+
+
+
+
